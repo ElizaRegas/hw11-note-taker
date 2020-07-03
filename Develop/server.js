a317cfb956095eeb937dce0cd8459fe1f3ea39a9
Eliza Regas <elizaregas@gmail.com>
2020-07-03 13:05:53 -0400

    added get and post routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,8 @@
 // Dependencies
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const { stringify } = require("querystring");
 
 // Setting up express and port
 const app = express();
@@ -9,12 +11,41 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// HTML routes
-app.get("/", function(req, res) {
+// HTML route
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/", "notes.html"));
+});
+
+// Api routes
+app.get("/api/notes", (req, res) => {
+  fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    return res.json(data);
+  });
+});
+
+
+app.post("/api/notes", (req, res) => {
+  fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    
+    const jsonData = JSON.parse(data);
+    const newNote = req.body;
+    jsonData.push(newNote);    
+    // return res.json(data);
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(jsonData), (err, data) => {
+      if (err) throw err;
+      console.log("File write succeeded");
+      res.json(jsonData);
+    })
+  });
+});
+
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/", "index.html"));
 });
 
 // Initiate the server
-app.listen(PORT, function() {
+app.listen(PORT, () => {
   console.log("App listening on PORT " + PORT);
 });
