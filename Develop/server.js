77e580119c73fec577336425c7c7cbc1270554d6
Eliza Regas <elizaregas@gmail.com>
2020-07-03 14:27:07 -0400

    all routes working now

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const { stringify } = require("querystring");
 
 // Setting up express and port
 const app = express();
@@ -11,12 +10,12 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// HTML route
+// Notes.html route
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/", "notes.html"));
 });
 
-// Api routes
+// Getting the json data
 app.get("/api/notes", (req, res) => {
   fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => {
     if (err) throw err;
@@ -24,23 +23,43 @@ app.get("/api/notes", (req, res) => {
   });
 });
 
-
+// Adding a new note to the json data
 app.post("/api/notes", (req, res) => {
   fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => {
     if (err) throw err;
     
     const jsonData = JSON.parse(data);
+    const idArr = jsonData.map(note => note.id);
+    const highestId = Math.max(...idArr);
     const newNote = req.body;
+    newNote.id = highestId + 1;
     jsonData.push(newNote);    
-    // return res.json(data);
+
     fs.writeFile(__dirname + "/db/db.json", JSON.stringify(jsonData), (err, data) => {
       if (err) throw err;
-      console.log("File write succeeded");
       res.json(jsonData);
     })
   });
 });
 
+// Deleting a note from the json data
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+
+  fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    
+    const jsonData = JSON.parse(data);
+    const results = jsonData.filter(note => note.id !== parseInt(noteId));
+
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(results), (err, data) => {
+      if (err) throw err;
+      res.json(results);
+    });
+  });
+});
+
+// Index.html route
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/", "index.html"));
 });
